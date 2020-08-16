869327ca68eb3a31416cea274ce94b27a84555a1
dragonfighter302 <vladimir.yeremin30@gmail.com>
2020-08-16 03:19:05 +0300

    Build files for GitHub pages

---
+++ precache-manifest.268782425154b0c503ca126400590f69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c31ae59a724f3d4189a0937c3c1066d",
+    "url": "/morse/index.html"
+  },
+  {
+    "revision": "76cfaa99a2c1a29d762d",
+    "url": "/morse/static/css/main.b04d5654.chunk.css"
+  },
+  {
+    "revision": "f454f29614553078e76c",
+    "url": "/morse/static/js/2.1a7f059a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/morse/static/js/2.1a7f059a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76cfaa99a2c1a29d762d",
+    "url": "/morse/static/js/main.9ac36744.chunk.js"
+  },
+  {
+    "revision": "5b7b6a755927a1c37319",
+    "url": "/morse/static/js/runtime-main.e4cb2133.js"
+  }
+]);
